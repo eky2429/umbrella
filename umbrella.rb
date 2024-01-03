65fc11a3595048c0e0ae6577abe6be710883d4f9
Ethan Ky <132713901+eky2429@users.noreply.github.com>
2024-01-03 17:35:15 +0000

    Made other changes to project

--- umbrella.rb
+++ umbrella.rb
@@ -11,5 +11,18 @@ puts "========================================"
 puts ""
 
 puts "Where are you?"
-input = gets.chomp
+#input = gets.chomp
+input = "Chicago"
 puts "Checking weather at #{input}..."
+
+puts GMAPS_KEY
+
+#url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + input + "&key=" + GMAPS_KEY
+#google_maps_info = JSON.parse(HTTP.get(url)).fetch("results").fetch("currently").at(0).fetch("geometry").fetch("location")
+#latitude = google_maps_info.fetch("lat")
+#puts latitude
+#longitude = google_maps_info.fetch("lng")
+#puts longitude
+#pp google_maps_info
+
+#pirate_weather_info = JSON.parse(HTTP.get("https://api.pirateweather.net/forecast/#{PIRATE_WEATHER_KEY}/41.8887,-87.6355"))
